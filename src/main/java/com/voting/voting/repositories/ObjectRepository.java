8dc3a48c9e3c1250d25a488724bf2f3e48076705
pbocer <przemyslaw.bocer@capgemini.com>
2020-05-31 17:10:48 +0200

    Change jpa repository to crud repository.

--- src/main/java/com/voting/voting/repositories/ObjectRepository.java
+++ src/main/java/com/voting/voting/repositories/ObjectRepository.java
@@ -2,10 +2,16 @@ package com.voting.voting.repositories;
 
 import com.voting.voting.entity.Object;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ObjectRepository extends JpaRepository<Object, Integer> {
+public interface ObjectRepository extends CrudRepository<Object, Long> {
+
+
+    List<Object> findByName(String name);
 
 }
 
